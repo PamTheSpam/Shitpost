094418846eb86796bf26676aa79cc54fc4e9cff5
PamTheSpam <70409171+PamTheSpam@users.noreply.github.com>
2021-01-07 21:51:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function cunt() {
+    document.getElementById("shitpost-paragraph").innerHTML = "Faggot";
+}
+
+document.getElementsById('sht-btn1').onclick = cunt();
+
+function normal() {
+    location.reload();
+}
+
+document.getElementsById('sht-btn2').onclick = normal();
