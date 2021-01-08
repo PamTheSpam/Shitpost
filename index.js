7988467afaa5c290a9dd09147f1652258cf419b0
PamTheSpam <70409171+PamTheSpam@users.noreply.github.com>
2021-01-08 13:16:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function cunt() {
-    document.getElementById("shitpost-paragraph").innerHTML = "Shticunt";
+    document.getElementById("shitpost-paragraph").innerHTML = "Shitcunt";
 }
 
 document.getElementsById('sht-btn1').onclick = cunt();
